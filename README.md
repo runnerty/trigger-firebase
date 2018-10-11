80c660ae7e12b49d0b5258b91401446f3e94ccb8
José Antonio Ruiz <jose@coderty.com>
2018-10-11 14:05:04 +0200

    feat(core): add firebase trigger

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Trigger Firebase
+#Firebase Realtime Database Trigger for [Runnerty]:
+
+With this trigger you can stream Firebase Realtime Database documents. It gets fired when a new record is stored in the document
+
+### Configuration sample:
+
+```json
+{
+  "id":"firebase_default",
+  "type":"@runnerty-trigger-firebase",
+  "serviceAccount": "/path-to-your-firebase-adminsdk.json",
+  "databaseURL": "https://your-firebase-project.firebaseio.com"
+}
+```
+
+### Plan sample to Stream a hashtag
+```json
+{
+  "id":"firebase_default",
+  "document":"/example/"
+}
+```
+
+### Chain input
+This trigger sends to the input of the chain all the data that is recorded in the database document.
+
+[Runnerty]: http://www.runnerty.io
---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+const admin = require("firebase-admin");
+var Trigger = global.TriggerClass;
+
+class triggerFirebase extends Trigger {
+  constructor(chain, params) {
+    super(chain, params);
+  }
+
+  start() {
+
+    let _this = this;
+    let serviceAccount = require(_this.params.config.serviceAccount);
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: _this.params.config.databaseURL,
+    });
+
+    let db = admin.database();
+    let initialDataLoaded = false;
+    let ref = db.ref(_this.params.document);
+
+    ref.on('child_added', function (snapshot) {
+      if (initialDataLoaded) {
+        const checkCalendar = true;
+        const inputValues = snapshot.val();
+        const customValues = {};
+
+        _this.startChain(checkCalendar, inputValues, customValues)
+          .then(() => { })
+          .catch(err => {
+            _this.logger.error("startChain error (triggerFirebase):", err);
+          });
+      }
+    });
+
+    ref.once('value', function (snapshot) {
+      initialDataLoaded = true;
+    });
+  }
+}
+
+module.exports = triggerFirebase;
